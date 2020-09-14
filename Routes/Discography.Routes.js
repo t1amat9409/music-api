bf33cba99a7a534ceff7fe4340b741d28e589057
CodeZAR <9409promos@gmail.com>
2020-09-14 11:48:15 +0200

    Update Discography.Routes.js
    Just the spaces, lol. Nothing much, if you're typescript you'll mind them all the time

--- Routes/Discography.Routes.js
+++ Routes/Discography.Routes.js
@@ -1,11 +1,11 @@
 
 const router = require('express').Router()
 const controller = require('../Controllers/Discography.Controller')
-const {uploadImage} = require('../Utils/uploader')
+const { uploadImage } = require('../Utils/uploader')
 
 router.get('/', controller.get)
-router.post('/',uploadImage,controller.add)
-router.get('/addpoints/:id',controller.addPoints)
-router.get('/artist/:id',controller.byArtist)
+router.post('/', uploadImage, controller.add)
+router.get('/addpoints/:id', controller.addPoints)
+router.get('/artist/:id', controller.byArtist)
 
 module.exports = router
