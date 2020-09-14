aca907bb388af3ab6cfe0f29e577992cd7210219
CodeZAR <9409promos@gmail.com>
2020-09-14 11:13:17 +0200

    Update Song.Controller.js
    I just felt like every child `func` can go inside the `Controller` object

--- Controllers/Song.Controller.js
+++ Controllers/Song.Controller.js
@@ -1,45 +1,33 @@
 const Song = require('../Models/Song.Model')
-const controller = {}
+const Controller = {
+    getByDisc: (req, res) =>{
+        Song.find({discography: req.params.id}).populate('discography').then(songResult =>{
+            res.status(200).json({data: songResult,message:'Songs found'})
+        }).catch(e =>{
+            res.status(500).json({error:e})
+        })
+    },
+    get: (req, res) =>{
+        Song.find().populate('discography').then(songResult =>{
+            res.status(200).json({data: songResult,message:'Songs found'})
+        }).catch(e =>{
+            res.status(500).json({error:e})
+        })
+    },
+    add: (req,res) =>{
+        const uploadedData = req.uploadedData
+        const {discography, title } = req.body
 
-controller.getByDisc = (req, res) =>{
+        const songObject = {
+            discography, title, url: uploadedData.url
+        }
 
-    Song.find({discography: req.params.id}).populate('discography').then(songResult =>{
-
-        res.status(200).json({data: songResult,message:'Songs found'})
-
-    }).catch(e =>{
-
-        res.status(500).json({error:e})
-
-    })
-
-}
-
-controller.get = (req, res) =>{
-    Song.find().populate('discography').then(songResult =>{
-        res.status(200).json({data: songResult,message:'Songs found'})
-    }).catch(e =>{
-        res.status(500).json({error:e})
-    })
-}
-
-controller.add = (req,res) =>{
-
-    const uploadedData = req.uploadedData
-    const {discography, title } = req.body
-
-    const songObject = {
-        discography, title, url: uploadedData.url
+        const newSong = new Song(songObject).save().then(songSaveResult =>{
+            res.status(201).json({data:songSaveResult,message:'Song saved'})
+        }).catch(e =>{
+            res.status(500).json({error:e})
+        })
     }
-
-    const newSong = new Song(songObject).save().then(songSaveResult =>{
-        res.status(201).json({data:songSaveResult,message:'Song saved'})
-    }).catch(e =>{
-        res.status(500).json({error:e})
-    })
-
-
-
 }
 
-module.exports = controller
+module.exports = Controller
